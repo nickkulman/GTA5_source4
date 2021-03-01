7b0246b0b29fb0836294f021ef965eeb083fced2
Nick Kulman <nickkulman@gmail.com>
2021-03-01 13:37:57 +0300

    Add files via upload

---
+++ GTA_4/main.js
@@ -0,0 +1,10 @@
+$('.list__close').on('click', function(){
+    $(this).addClass('selected');
+    
+    });
+
+
+$('.list__icon').on('click', function(){
+    $(this).addClass('checked');
+                
+    });
